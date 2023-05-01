466870d8327e981d7c9b9652e5d9e83ebd05def2
Liu Ye <1316519465@qq.com>
2023-05-01 21:46:25 +0800

    add multi redis resource part

--- src/main/resources/sql/data.sql
+++
@@ -1,3 +0,0 @@
-insert into users values
-(1, "Mary", 18, "test@test.com"),
-(2, "Tom", 10, "admin@example.com");
--- src/main/resources/sql/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists users;
-create table users
-(
-    id int auto_increment,
-    name varchar(64),
-    age int,
-    email varchar(64),
-    primary key (id)
-);
